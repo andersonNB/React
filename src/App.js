8744dcac56da9aba0af2b4ea7772d4f545010688
andersonNB <andersonaugustonb@ufps.edu.co>
2021-06-12 11:32:38 -0500

    Aprendiendo_react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/image/logo.svg';
+import './assets/css/App.css';
+
+function imprimirPresentacion(nombre,edad){
+
+  var presentacion = 
+  <div>
+    <h2>Soy un {nombre}</h2>
+    <h3>Tengo una edad de {edad} años</h3>
+  </div>
+  
+  return presentacion
+}
+
 
 function App() {
+
+  var nombre = "Ejemplo"
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Web page
         </p>
+        {imprimirPresentacion(nombre,24)}
         <a
           className="App-link"
           href="https://reactjs.org"
